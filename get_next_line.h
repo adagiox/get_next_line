5cb3efed63cb0c5ebf71c5f0208fca269a2ac11e
Erik Rintala <erik.rintala@gmail.com>
2017-04-25 02:28:37 -0700

    Added a struct for holding data

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,10 @@
 #include <stdlib.h>
 
-#define BUF_SIZE 1
+// List for holding lines
+
+typedef struct		s_line
+{
+	unsigned char	*content;
+	unsigned int	content_size;
+	struct s_line	*next;
+}					t_line;

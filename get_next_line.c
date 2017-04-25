c68e6667334a537d8a674c37c92e0076a1d1c69f
Erik Rintala <erik.rintala@gmail.com>
2017-04-25 02:29:27 -0700

    Added a struct for holding data

--- get_next_line.c
+++ get_next_line.c
@@ -1,24 +1,6 @@
 #include "get_next_line.h"
 
-int		get_next_line()
-{
-	int	bytes_read;
 
-	bytes_read = 1;
-	return (bytes_read);
-}
+// init node
 
-int		line_size(char *buf)
-{
-	int line;
-
-	line = 0;
-	while (*buf)
-	{
-		if (*buf == '\n')
-			return (line);
-		else
-			line++;
-	}
-	return (line);
-}
+// build list from data
